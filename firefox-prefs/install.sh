ff49c10681f84fc64e9f955d2889e964f7cc22fa
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2016-11-10 22:50:52 +0000

    dumb mistake

--- firefox-prefs/install.sh
+++ firefox-prefs/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for f in $(awk -F = '/Path/ {print $2}'  $HOME/.mozilla/firefox/profiles.ini); do
-    ln -sr ./user.js "$HOME/.mozilla/firefox/$f"
-do
+    ln -fsr ./user.js "$HOME/.mozilla/firefox/$f"
+done
