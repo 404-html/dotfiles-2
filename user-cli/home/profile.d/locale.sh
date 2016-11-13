bfc3323640273a465fedefff4190e1fb7be5fe76
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2016-11-13 15:26:50 +0000

    LC_ALL so I don't forget

--- user-cli/home/profile.d/locale.sh
+++ user-cli/home/profile.d/locale.sh
@@ -1,6 +1,7 @@
 
 # English language, iso-8601 date format, euro money, metric system
 #export LANG=en_US.UTF-8
+#export LC_ALL="en_DK.utf8"
 #export LC_CTYPE="en_US.utf8"
 #export LC_NUMERIC="en_US.utf8"
 #export LC_TIME="en_DK.utf8"
