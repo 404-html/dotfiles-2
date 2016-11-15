0ea72c43e097a216ab753e4ed936db9559029943
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2016-11-15 08:33:26 +0000

    Bug fix

--- user-cli/home/bin/conkyrc_cron.sh
+++ user-cli/home/bin/conkyrc_cron.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p /tmp/$USER_conky
+exec 2>/dev/null
 
-curl -4 wttr.in | sed -n '3,7p' | perl -pe 's/\e\[?.*?[\@-~]//g' > /tmp/$USER_conky/weather 2>/dev/null
-apt list --upgradable 2>/dev/null | tail -n +2 | wc -l > /tmp/$USER_conky/apt_ulist 2>/dev/null
-dig +short myip.opendns.com @resolver1.opendns.com > /tmp/$USER_conky/wanip 2>/dev/null
+dir=/tmp/${USER}_conky
+mkdir -p "${dir}"
+
+curl -4 wttr.in | sed -n '3,7p' | perl -pe 's/\e\[?.*?[\@-~]//g' > "${dir}/weather"
+apt list --upgradable | tail -n +2 | wc -l > "${dir}/apt_ulist"
+dig +short myip.opendns.com @resolver1.opendns.com > "${dir}/wanip"
 
