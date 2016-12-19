936ef63b56941eba6d6d82cc95c12d4512532089
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2016-12-19 20:03:16 +0000

    auto remove broken links on install

--- user-gui/install.sh
+++ user-gui/install.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+find "$HOME" -maxdepth 1 &>/dev/null && echo Searching for broken links
+find "$HOME" -maxdepth 1 -xtype l -ok rm '{}' \;
 cd home
 for f in *;do
     if [ -e "$f" ];then
@@ -11,6 +13,8 @@ done
 cd - >/dev/null
 
 for d in config local/share ;do
+    find "$HOME/.$d" -maxdepth 1 &>/dev/null && echo Searching for broken links
+    find "$HOME/.$d" -maxdepth 1 -xtype l -ok rm '{}' \;
     cd "$d"
     for f in *;do
         if [ -e "$f" ]; then
