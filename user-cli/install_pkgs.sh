38bd424f53eee87277dcffc59139202b82f731b7
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2016-12-22 15:44:29 +0000

    alias tweaks, removed old bins

--- user-cli/install_pkgs.sh
+++ user-cli/install_pkgs.sh
@@ -18,7 +18,7 @@ if hash apt-get &>/dev/null; then
     pkgs=(neovim htop curl git grc ssvnc 
         xclip xcape x11vnc sudo xcape pdfgrep
         trash-cli dnsutils python3-pyftpdlib inxi
-        tmux mpv wine wine-development libsecret-tools
+        tmux mpv wine wine-development libsecret-tools neofetch
         youtube-dl net-tools ffmpeg silversearcher-ag command-not-found)
 
     "$tool" install "${pkgs[@]}"
