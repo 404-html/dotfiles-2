5693371fff43d66fed5b5bd944b0f36a1adf4fdc
Pedro Gameiro <pedro.a.gameiro@tecnico.ulisboa.pt>
2017-01-14 20:35:45 +0000

    .

--- user-cli/home/bin/prep_motog.sh
+++ user-cli/home/bin/prep_motog.sh
@@ -2,8 +2,8 @@
 
 adb root
 adb remount
-echo chmod 000 -R /system/app/Stk | adb shell
-echo chmod 000 -R /system/media/audio/notifications/camera_click.ogg | adb shell
-echo chmod 000 -R /system/media/audio/notifications/camera_focus.ogg | adb shell
+adb shell chmod 000 -R /system/app/Stk
+adb shell chmod 000 -R /system/media/audio/ui/camera_click.ogg
+adb shell chmod 000 -R /system/media/audio/ui/camera_focus.ogg
 
 echo 'Done. Modification will apply after next reboot.'
