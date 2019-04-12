e251cce09f68120a91c5fd1765daad304212b915
Pedro Gameiro <pedro.gameiro@freiheit.com>
2019-04-12 16:36:53 +0100

    fix bug

--- user-cli/home/shrc.d/30-kubectl.sh
+++ user-cli/home/shrc.d/30-kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -e /usr/bin/kubectl ] || return
+hash kubectl 2>/dev/null || return
 
 if [[ "$(ps -p$$ -ocmd=)" =~ .*zsh.* ]]; then
     source <(kubectl completion zsh)
@@ -10,3 +10,4 @@ fi
 
 export KUBECONFIG="$(ls $HOME/.kube/config* | tr '\n' ':' | sed -e 's/:$//g')"
 alias k=kubectl
+
