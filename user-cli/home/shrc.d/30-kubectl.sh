9f4d80c52485e807c92370d22750b28a1c23f366
Pedro Gameiro <pedro.gameiro@freiheit.com>
2019-04-13 09:59:49 +0100

    fix bug when ~/.kube is empty or non existant

--- user-cli/home/shrc.d/30-kubectl.sh
+++ user-cli/home/shrc.d/30-kubectl.sh
@@ -2,12 +2,15 @@
 
 hash kubectl 2>/dev/null || return
 
+
 if [[ "$(ps -p$$ -ocmd=)" =~ .*zsh.* ]]; then
     source <(kubectl completion zsh)
 elif [[ "$(ps -p$$ -ocmd=)" =~ .*bash.* ]]; then
     source <(kubectl completion bash)
 fi
 
-export KUBECONFIG="$(ls $HOME/.kube/config* | tr '\n' ':' | sed -e 's/:$//g')"
-alias k=kubectl
+if [ -d $HOME/.kube ] && [ "$(ls $HOME/.kube/)" ]; then
+    export KUBECONFIG="$(echo -n $HOME/.kube/config* | tr ' ' ':')"
+fi
 
+alias k=kubectl
